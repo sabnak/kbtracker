98fc6186946f8ae2c475c78a78185ae529d1391b
yithian <yithiann@yandex.ru>
2026-01-26 14:46:18 +0300

    preparing for inventory parsing

--- src/utils/parsers/save_data/SaveFileData.py
+++ src/utils/parsers/save_data/SaveFileData.py
@@ -3,6 +3,15 @@ import typing
 import pydantic
 
 
-class SaveFileData(pydantic.BaseModel):
+class GameObjectData(pydantic.BaseModel):
+	kb_id: str
+	quantity: int | None = None
 
+
+class HeroInventory(pydantic.BaseModel):
+	items: list['GameObjectData']
+
+
+class SaveFileData(pydantic.BaseModel):
 	shops: list[dict[str, typing.Any]]
+	hero_inventory: HeroInventory | None = None
